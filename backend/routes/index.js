7093aa90647b8206feb129a308da7f48f206949a
Aashu-Crypto <ashugandotra14@gmail.com>
2024-03-27 22:10:30 +0530

    backend

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserLoginRouter = require("./UserLogin");
-const productRouter = require("./productDetails");
-router.use("/user", UserLoginRouter);
-router.use("/products", productRouter);
-
-module.exports = router;
-
--- frontend/src/components/LandingPage/Todays.jsx
+++ frontend/src/components/LandingPage/Todays.jsx
@@ -1,29 +1,18 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-import {backendHost} from '../ApiConfig'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { backendHost } from "../ApiConfig";
 
 const Todays = () => {
-
-
-  useEffect(()=>{
-
+  useEffect(() => {
     fetch(`${backendHost}/products/details`)
-        // .then(res => JSON.parse(res))
-        .then((res) => res.json())
-        .then((json) => {
-  
-        console.log(json)
-        });
-  
-  },[])
-  
-  return (
-    <div>
-      Today's Featured
-    </div>
-  )
-}
+      // .then(res => JSON.parse(res))
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }, []);
 
-export default Todays
+  return <div>Today's Featured</div>;
+};
+
+export default Todays;
